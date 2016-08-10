a54d0e37827a96664a4fbc7c26b636818898db63
HardHatArea <jon.hoopes@gmail.com>
2016-08-09 21:47:55 -0600

    end of tuesday class august 9

--- public/js/routes.js
+++ public/js/routes.js
@@ -6,11 +6,34 @@ angular.module('practice2').config(function($stateProvider, $urlRouterProvider) 
       templateUrl: "/views/home.html",
       controller: 'mainCtrl'
     })
+    .state('about', {
+      url: "/about",
+      templateUrl: "/views/about.html",
+    })
+    .state('account', {
+      url: "/account",
+      templateUrl: "/views/account.html",
+    })
+    .state('cart', {
+      url: "/cart",
+      templateUrl: "/views/cart.html",
+    })
+    .state('dribbling', {
+      url: "/dribbling",
+      templateUrl: "/views/dribbling.html",
+    })
     .state('products', {
       url: "/products",
       templateUrl: "/views/products.html",
     })
+    .state('shooting', {
+      url: "/shooting",
+      templateUrl: "/views/shooting.html",
+    })
+    .state('wall', {
+      url: "/wall",
+      templateUrl: "/views/wall.html",
+    })
 
     $urlRouterProvider.otherwise("/")
-
 })
